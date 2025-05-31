c9de323d70131a04deabb7461b3a9c3d414c7ae3
ocki26 <ocki2zz65@gmail.com>
2025-05-31 23:13:44 +0700

    day 7

--- vite-project/src/Component/Form.jsx
+++ vite-project/src/Component/Form.jsx
@@ -1,20 +1,37 @@
 import { use, useState } from "react";
-const [name, setName] = useState("");
+
 const Form = () => {
-  function handleChange(e) {
-    console.log(e);
-  }
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  // function handleChange(e) {
+  //   setName(e.target.value);
+  //   console.log(name)
+  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
   return (
     <>
       <div>
+        {name.fistName}-{name.lastName}
         <form>
           <input
-            onChange={function demo(e) {
-              return handleChange(e);
-            }}
+            // onChange={function demo(e) {
+            //   return handleChange(e);
+            // }}
+            // onChange={(e) => handleChange(e)}
+            // type="text"
+            // value={name}
+            onChange={(e) => setName({ ...name, firstName: e.target.value })}
             type="text"
-            value={name}
+            value={name.fistName}
           />
+          <input
+            type="text"
+            onChange={(e) => setName({ ...name, lastName: e.target.value })}
+            value={name.lastName}
+          />
+          <button onClick={(e) => handleSubmit(e)}>Add</button>
         </form>
       </div>
     </>
